0c4d2cf4d11d16fad4feaa241b9581124130243d
Jay P <wainwright7@users.noreply.github.com>
2022-06-08 17:29:11 +0000

    working on if, elif, else logic

---
+++ simpsonslice.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Morning Slicing Challenge!"""
+
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+
+def main():
+    """write your code in this function to solve the challenge"""
+
+    # write one print function for each list above
+    
+    # get the strings "eyes," "goggles," and "nothing from each
+
+    # final output for each print should look like this:
+    e = trial[2]["goggles"]
+    g = trail[2]["eyes"]
+    n = trail[3]
+
+    """My eyes! The goggles do nothing!"""
+
+    print(f"My {challenge[2][1]! The {challenge[2][0] do {challenge[3]!")
+    # print()
+    # print()
+
+
+
