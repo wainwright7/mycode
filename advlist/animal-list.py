f36235d285e5c134369513188142296692de73e4
wainwright7 <jaypatelus7@gmail.com>
2022-09-28 22:33:27 +0000

    About complex list

---
+++ advlist/animal-list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python Alta3 | Jay
+   Learning complex List"""
+
+def main():
+
+    #creating a new list
+    list1 = ["Cat","Fox", "Cow", "Hen", "Jay"]
+    print(list1)
+
+    print(list1[0], list1[1], list1[2], list1[3], list1[4])
+
+    print(' ' .join(list1))
+
+    print(' - '.join(list1))
+
+
+main()
---
+++ lab_input/chal01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Challange 01 | Jay
+   Practicing input , print"""
+
+def main():
+
+    user_name = input("What is your name?: ")
+
+    day_today = input("What day of the week is today?: ")
+
+    print(f"Hello, {user_name}! Happy {day_today}!")
+
+main()
